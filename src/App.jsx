3ea2ed42f4c78fe151eefa12ebe99a0ee97ae023
RISHIRAJ1360 <rishiraj1360@gmail.com>
2025-06-24 18:20:19 +0530

    hello world

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import './App.css'
+import { Analytics } from "@vercel/analytics/react"
 import Header from './components/Header'
 import Hero from './sections/Hero'
 import Services from './sections/Services'
